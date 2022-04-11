2dd1b27e73d2c62492f0812713344472e69eb858
Yagna08 <93083732+Yagna08@users.noreply.github.com>
2022-04-11 23:00:43 +0530

    Create RevStrDAY1.cpp

---
+++ RevStrDAY1.cpp
@@ -0,0 +1,15 @@
+
+string reverseWord(string str){
+    int i=0,j=str.length()-1;
+    char temp;
+    while(i<j)
+    {
+        temp=str[i];
+        str[i++]=str[j];
+        str[j--]=temp;
+        
+    }
+    return str;
+  //Your code here
+}
+
