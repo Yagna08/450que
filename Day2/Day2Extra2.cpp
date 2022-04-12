a8d3bd91bff137df5a3a1fb4a70b806826f91ae2
Yagna08 <93083732+Yagna08@users.noreply.github.com>
2022-04-12 16:25:18 +0530

    Create Day2Extra2.cpp

---
+++ Day2/Day2Extra2.cpp
@@ -0,0 +1,20 @@
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long long maxSubarraySum(int arr[], int n){
+        long long int sum=0;
+        // Your code here
+        long long int maxi=arr[0];
+        for(int i=0;i<n;i++)
+        {
+            sum+=arr[i];
+            if(sum>maxi)
+            maxi=sum;
+            if(sum<0)
+            sum=0;
+        }
+        return maxi;
+    }
+};
